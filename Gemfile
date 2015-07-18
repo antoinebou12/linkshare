7c71302bfd7a16c745e993aa4d31d5a09065d878
Antoine <antoine_boucher@outlook.com>
2015-07-18 15:22:34 +0000

    Add comments

--- Gemfile
+++ Gemfile
@@ -17,3 +17,4 @@ gem 'spring',        group: :development
 gem 'sqlite3'
 gem 'bootstrap-sass', '~> 3.2.0.2'
 gem 'acts_as_votable', '~> 0.10.0'
+gem 'simple_form', '~>3.0.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :links do
   member do
     put "like", to: "links#upvote"
     put "dislike", to: "links#downvote"
   end
+  resources :comments
 end
   root "links#index"
   
